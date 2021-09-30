ff28a2428a8b30eff10575e8037f990881fdfdc5
Alessandro Bellafronte <alessandro@4eyes.ch>
2021-09-30 11:39:45 +0200

    BBI-8: Fixed major issues by reverting to functional component for now

--- src/viewer/image/ReactMirador.tsx
+++ src/viewer/image/ReactMirador.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import i18next from 'i18next';
 import './mirador.css';
 import IManifestData from "../../interface/IManifestData";
@@ -7,65 +7,40 @@ import Mirador from 'mirador';
 import ocrHelperPlugin from '@4eyes/mirador-ocr-helper';
 
 interface IProps {
-  manifest: IManifestData;
+    manifest: IManifestData;
 }
-interface IState {
-  manifest: IManifestData;
-}
-export default class ReactMirador extends React.Component<IProps, IState> {
 
-  private miradorInstance: any;
+export default function ReactMirador({manifest}: IProps) {
+    // init viewer
+    useEffect(() => {
+        const config = {
+            id: 'mirador',
+            workspace: {
+              allowNewWindows: false,
+              isWorkspaceAddVisible: false
+            },
+            window: {
+                allowClose: true,
+                textOverlay: {
+                  enabled: true,
+                  visible: true,
+                },
+                sideBarOpenByDefault: true,
+                panels: {
+                    info: true
+                }
+            },
+            windows: [
+              {
+                manifestId: manifest.id,
+              },
+            ],
+            language: i18next.language
+          };
+          
+          Mirador.viewer(config, [...ocrHelperPlugin]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[]);
 
-  constructor(props: IProps) {
-
-    super(props);
-
-    this.state = {
-      manifest: props.manifest
-    };
-  }
-  // init viewer
-  componentDidMount() {
-    const config = {
-      id: 'mirador',
-      workspace: {
-        allowNewWindows: false,
-        isWorkspaceAddVisible: false
-      },
-      window: {
-        allowClose: true,
-        textOverlay: {
-          enabled: true,
-          visible: true,
-        },
-        sideBarOpenByDefault: true,
-        panels: {
-          info: true
-        }
-      },
-      windows: [
-        {
-          canvasIndex: 8,
-          manifestId: this.state.manifest.id,
-        },
-      ],
-      language: i18next.language
-    };
-
-    this.miradorInstance = Mirador.viewer(config, [...ocrHelperPlugin]);
-
-    // i18next.on('languageChanged', this.languageChanged.bind(this))
-  };
-
-  languageChanged() {
-    console.log({i: this.miradorInstance})
-
-    this.miradorInstance.store.dispatch({type: 'UPDATE_CONFIG',
-      language: i18next.language,
-    });
-  }
-
-  render() {
-    return <div id={'mirador'} className="aiiif-mirador" key={this.state.manifest.id}></div>;
-  }
+    return <div id={'mirador'} className="aiiif-mirador" key={manifest.id}></div>;
 }
