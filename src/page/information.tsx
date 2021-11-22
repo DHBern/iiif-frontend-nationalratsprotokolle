2679a993e5c131e7c0506b3c8057a11da540b7aa
Loris <loris@4eyes.ch>
2021-11-22 13:15:56 +0100

    BBI-9 More textual info from customers

--- src/page/information.tsx
+++ src/page/information.tsx
@@ -9,7 +9,27 @@ export default function PageOverview() {
                 <Main>
                     <>
                         <h1>{t('pageInformationH1')}</h1>
-                        <p>To be defined</p>
+                        <p>{t('infoIntroP1')}</p>
+                        <p>{t('infoIntroP2')}</p>
+                        <p>{t('infoIntroP3')}</p>
+
+                        <h2>{t('infoSearchH2')}</h2>
+                        <p>{t('infoSimpleP1')}</p>
+                        <p>{t('infoSimpleP2')}</p>
+                        <p>{t('infoSimpleP3')}</p>
+                        <ul>
+                            <li>{t('infoSimpleL1')}</li>
+                            <li>{t('infoSimpleL2')}</li>
+                            <li>{t('infoSimpleL3')}</li>
+                        </ul>
+
+                        <h2>{t('infoResultH2')}</h2>
+                        <p>{t('infoResultP1')}</p>
+                        <p>{t('infoResultP2')}</p>
+
+                        <h2>{t('navItemOverview')}</h2>
+                        <p>{t('infoBrowserP1')}</p>
+                        <span>IMAGE TODO</span>
                     </>
                 </Main>
             )}
