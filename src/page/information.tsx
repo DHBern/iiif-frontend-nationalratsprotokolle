a2108958434be45c3dec245d65cf597e8bccb11c
Loris <loris@4eyes.ch>
2021-11-22 16:37:43 +0100

    BBI-9 Added target and noreferrer thanks to Ale

--- src/page/information.tsx
+++ src/page/information.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 import { Translation } from 'react-i18next';
 import Main from "../main/main";
 import * as DOMPurify from "dompurify";
-import {getLocalized, sanitizeRulesSet} from "../lib/ManifestHelpers";
 import browserImage from "../images/scrn-browser.png";
 import miradorImage from "../images/scrn-mirador.png";
 
 export default function PageOverview() {
+    DOMPurify.addHook('afterSanitizeAttributes', function (node: any) {
+        // set all elements owning target to target=_blank
+        if ('target' in node) {
+            node.setAttribute('target', '_blank');
+            node.setAttribute('rel', 'noopener');
+        }
+    });
     return (
         <Translation ns="common">
             {(t) => (
@@ -14,43 +20,43 @@ export default function PageOverview() {
                     <>
                         <h1>{t('pageInformationH1')}</h1>
                         <div dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-                            __html: DOMPurify.sanitize(t('infoIntro'), sanitizeRulesSet)
+                            __html: DOMPurify.sanitize(t('infoIntro'))
                         }} />
 
                         <h2>{t('infoSearchH2')}</h2>
                         <div dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-                            __html: DOMPurify.sanitize(t('infoSimple'), sanitizeRulesSet)
+                            __html: DOMPurify.sanitize(t('infoSimple'))
                         }} />
                         <ul dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-                            __html: t('infoSimpleList')
+                            __html: DOMPurify.sanitize(t('infoSimpleList'))
                         }}>
                         </ul>
 
                         <h2>{t('infoResultH2')}</h2>
                         <div dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-                            __html: DOMPurify.sanitize(t('infoResult'), sanitizeRulesSet)
+                            __html: DOMPurify.sanitize(t('infoResult'))
                         }} />
 
                         <h2>{t('navItemOverview')}</h2>
                         <div dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-                            __html: DOMPurify.sanitize(t('infoBrowserBeforeImage'), sanitizeRulesSet)
+                            __html: DOMPurify.sanitize(t('infoBrowserBeforeImage'))
                         }} />
                         <img alt={t('infoBrowserImageAlt')} src={browserImage} />
                         <div className="italic" dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-                            __html: DOMPurify.sanitize(t('infoBrowserImageAlt'), sanitizeRulesSet)
+                            __html: DOMPurify.sanitize(t('infoBrowserImageAlt'))
                         }} />
 
                         <h2>{t('infoMiradorH2')}</h2>
                         <div dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-                            __html: DOMPurify.sanitize(t('infoMirador'), sanitizeRulesSet)
+                            __html: DOMPurify.sanitize(t('infoMirador'))
                         }} />
                         <ul dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-                            __html: t('infoMiradorList')
+                            __html: DOMPurify.sanitize(t('infoMiradorList'))
                         }}>
                         </ul>
                         <img alt={t('infoMiradorImageAlt')} src={miradorImage} />
                         <div className="italic" dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
-                            __html: DOMPurify.sanitize(t('infoMiradorImageAlt'), sanitizeRulesSet)
+                            __html: DOMPurify.sanitize(t('infoMiradorImageAlt'))
                         }} />
                     </>
                 </Main>
