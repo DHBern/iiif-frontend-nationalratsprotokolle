e53987c40f212b4493472981be8ae4a0bb92b94a
Loris <loris@4eyes.ch>
2021-11-22 14:36:13 +0100

    BBI-9 Added more information content

--- src/page/information.tsx
+++ src/page/information.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Translation } from 'react-i18next';
 import Main from "../main/main";
+import * as DOMPurify from "dompurify";
+import {getLocalized, sanitizeRulesSet} from "../lib/ManifestHelpers";
+import browserImage from "../images/scrn-browser.png";
+import miradorImage from "../images/scrn-mirador.png";
 
 export default function PageOverview() {
     return (
@@ -9,27 +13,45 @@ export default function PageOverview() {
                 <Main>
                     <>
                         <h1>{t('pageInformationH1')}</h1>
-                        <p>{t('infoIntroP1')}</p>
-                        <p>{t('infoIntroP2')}</p>
-                        <p>{t('infoIntroP3')}</p>
+                        <div dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+                            __html: DOMPurify.sanitize(t('infoIntro'), sanitizeRulesSet)
+                        }} />
 
                         <h2>{t('infoSearchH2')}</h2>
-                        <p>{t('infoSimpleP1')}</p>
-                        <p>{t('infoSimpleP2')}</p>
-                        <p>{t('infoSimpleP3')}</p>
-                        <ul>
-                            <li>{t('infoSimpleL1')}</li>
-                            <li>{t('infoSimpleL2')}</li>
-                            <li>{t('infoSimpleL3')}</li>
+                        <div dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+                            __html: DOMPurify.sanitize(t('infoSimple'), sanitizeRulesSet)
+                        }} />
+                        <ul dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+                            __html: t('infoSimpleList')
+                        }}>
                         </ul>
 
                         <h2>{t('infoResultH2')}</h2>
-                        <p>{t('infoResultP1')}</p>
-                        <p>{t('infoResultP2')}</p>
+                        <div dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+                            __html: DOMPurify.sanitize(t('infoResult'), sanitizeRulesSet)
+                        }} />
 
                         <h2>{t('navItemOverview')}</h2>
-                        <p>{t('infoBrowserP1')}</p>
-                        <span>IMAGE TODO</span>
+                        <div dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+                            __html: DOMPurify.sanitize(t('infoBrowserBeforeImage'), sanitizeRulesSet)
+                        }} />
+                        <img alt={t('infoBrowserImageAlt')} src={browserImage} />
+                        <div className="italic" dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+                            __html: DOMPurify.sanitize(t('infoBrowserImageAlt'), sanitizeRulesSet)
+                        }} />
+
+                        <h2>{t('infoMiradorH2')}</h2>
+                        <div dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+                            __html: DOMPurify.sanitize(t('infoMirador'), sanitizeRulesSet)
+                        }} />
+                        <ul dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+                            __html: t('infoMiradorList')
+                        }}>
+                        </ul>
+                        <img alt={t('infoMiradorImageAlt')} src={miradorImage} />
+                        <div className="italic" dangerouslySetInnerHTML={{ // eslint-disable-line react/no-danger
+                            __html: DOMPurify.sanitize(t('infoMiradorImageAlt'), sanitizeRulesSet)
+                        }} />
                     </>
                 </Main>
             )}
