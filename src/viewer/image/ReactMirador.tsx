a7f66665825ac1d8c9e86d92a3586eedfa389de9
Alessandro Bellafronte <alessandro@4eyes.ch>
2022-10-18 13:53:54 +0200

    BRP-51: Refactoring of ReactMirador; adding search support

--- src/viewer/image/ReactMirador.tsx
+++ src/viewer/image/ReactMirador.tsx
@@ -7,6 +7,7 @@ import ocrHelperPlugin from '@4eyes/mirador-ocr-helper';
 // https://github.com/ProjectMirador/mirador/commit/2759fccc641b40b9fff0b9da5ef83d6ecd0e3dd2#diff-90d821e4b96f948716bc831f305dea317c3c9e49c8270798d85017859044de0c
 //@ts-ignore
 import * as actions from 'mirador/dist/es/src/state/actions/index.js';
+import * as selectors from 'mirador/dist/es/src/state/selectors/index.js';
 
 import { AppContext } from "../../AppContext";
 import Config from "../../lib/Config";
@@ -15,168 +16,197 @@ declare let global: {
     config: Config;
 };
 
+// Defines the mobile breakpoint for the Mirador viewer
+const MOBILE_BREAKPOINT = 600;
+
 export default function ReactMirador() {
-    const id = useRef<number>(Math.floor(Math.random() * 10000));
+    const id = useRef<number>(Math.floor(Math.random() * 10000)).current.toString(10);
     const [viewerInstance, setViewerInstance] = useState<any>(null);
-    const [isMobile, setIsMobile] = useState<boolean>(window.matchMedia('(max-width: 600px)').matches);
+    const [isMobile, setIsMobile] = useState<boolean>(window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT}px)`).matches);
     const { currentManifest } = useContext(AppContext);
     const { searchParams } = new URL(window.location.href);
     const canvasImageName = searchParams.has('cv') ? searchParams.get('cv') : '';
     const collectionParts = canvasImageName?.split('-');
     const collectionName = collectionParts ? collectionParts[0] : '';
-    const canvasId = `${process.env.REACT_APP_MANIFEST_API_BASE}${collectionName}/canvasses/${canvasImageName}`;
+    const canvasId = `${process.env.REACT_APP_IMAGE_API_BASE}/${collectionName}%2F${canvasImageName}.jpg`;
     const availableLanguages = global.config.getTranslations();
+    const isMobileHandler = (event: any) => setIsMobile(event.matches);
 
-    // init viewer
     useEffect(() => {
-        let windows = {};
+        // Initializing Mirador
+        let config = {
+            id: `mirador-${id}`,
+            createGenerateClassNameOptions: {
+                // Options passed directly to createGenerateClassName in Material-UI https://material-ui.com/styles/api/#creategenerateclassname-options-class-name-generator
+                productionPrefix: `mirador-${id}`,
+            },
+            workspace: {
+                allowNewWindows: false,
+                isWorkspaceAddVisible: false,
+            },
+            window: {
+                allowFullscreen: true,
+                allowClose: true,
+                textOverlay: {
+                    enabled: !isMobile,
+                    visible: !isMobile,
+                },
+                sideBarOpenByDefault: !isMobile,
+                panels: {
+                    info: true,
+                    annotations: false,
+                },
+            },
+            windows: [],
+            thumbnailNavigation: {
+                defaultPosition: isMobile ? 'off' : 'far-right',
+            },
+            language: i18next.language,
+            availableLanguages: false, // Workaround: Needs to be 'false' on init, otherwise restrictions won't be applied...
+        };
 
-        if (currentManifest && currentManifest.id && isMobile !== undefined) {
-            if (searchParams.has('cv')) {
-                windows = {
-                    loadedManifest: searchParams.get('manifest'),
-                    canvasId,
-                };
-            } else {
-                windows = {
-                    manifestId: currentManifest.id
-                }
-            }
-            const config = {
-                id: 'mirador-' + id.current.toString(10),
-                createGenerateClassNameOptions: { // Options passed directly to createGenerateClassName in Material-UI https://material-ui.com/styles/api/#creategenerateclassname-options-class-name-generator
-                    productionPrefix: 'mirador-' + id.current.toString(10),
-                },
-                workspace: {
-                    allowNewWindows: false,
-                    isWorkspaceAddVisible: false
-                },
-                window: {
-                    allowFullscreen: true,
-                    allowClose: true,
-                    textOverlay: {
-                        enabled: !isMobile,
-                        visible: !isMobile,
-                    },
-                    sideBarOpenByDefault: !isMobile,
-                    panels: {
-                        info: true,
-                        annotations: false,
-                    }
-                },
-                thumbnailNavigation: {
-                    defaultPosition: isMobile ? 'off' : 'far-right',
-                },
-                windows: [
-                    windows,
-                ],
-                language: i18next.language,
-                availableLanguages: false, // Workaround: Needs to be 'false' on init, otherwise restrictions won't be applied...
-            };
+        setViewerInstance(Mirador.viewer(config, [...ocrHelperPlugin]));
 
-            setViewerInstance(Mirador.viewer(config, [...ocrHelperPlugin]));
-
-            return () => {
-                setViewerInstance(null);
-            }
-        }
+        /**
+         * This method is executed when the component is unmounted (=destructor)
+         */
+         return () => {
+            setViewerInstance(null);
+        };
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [currentManifest]);
+    }, []);
 
+    /**
+     * This method is executed when viewerInstance has been created
+     */
     useEffect(() => {
+        // As soon as the viewerInstance is available, we can apply the available languages
         if (viewerInstance) {
             const { store } = viewerInstance;
             const state = store.getState();
-            if (searchParams.has('q')) {
-                const targetId = Object.keys(state.windows)[0];
-                const companionWindows: any = Object.values(state.companionWindows)
-                    .filter((obj: any) => obj.content !== 'thumbnailNavigation');
-    
-                new Promise((resolve) => resolve(
-                    store.dispatch(
-                        actions.updateCompanionWindow(
-                            targetId,
-                            companionWindows[0].id,
-                            {
-                                content: 'search',
-                                position: 'left'
-                            }
-                        )
-                    )
-                )).then(() => {
-                    const searchUrl = `${process.env.REACT_APP_MANIFEST_SEARCH_URL}/${collectionName}?q=${searchParams.get('q')}`;
-    
-                    new Promise((resolve) => resolve(
-                        store.dispatch(
-                            actions.fetchSearch(
-                                targetId,
-                                companionWindows[0].id,
-                                searchUrl
-                            )
-                        )
-                    )).then(() => {
-                        store.dispatch(
-                            actions.setCanvas(
-                                targetId,
-                                canvasId
-                            )
-                        );
+
+            if (!state.config.availableLanguages) {
+                store.dispatch(
+                    actions.updateConfig({
+                        availableLanguages,
                     })
-                });
+                );
             }
-            if(!state.config.availableLanguages) {
-                viewerInstance.store.dispatch(actions.updateConfig({
-                    availableLanguages
-                }));
-            }
+
+            // Additionally, several events listeners are defined...
+            i18next.on('languageChanged', changeLanguage);
+            window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT}px)`).addEventListener('change', isMobileHandler);
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        /**
+         * This method is executed when the component is unmounted (=destructor)
+         */
+        return () => {
+            i18next.off('languageChanged', changeLanguage);
+            window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT}px)`).removeEventListener('change', isMobileHandler);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [viewerInstance]);
 
+    /**
+     * This method is executed when viewerInstance has been created and every time when the currentManifest changes
+     */
     useEffect(() => {
+        // Every time the currentManifest changes, we need to update the mirador windows to display the new manifest
+        if (viewerInstance && currentManifest) {
+            const { store } = viewerInstance;
+            const searchUrl = `${process.env.REACT_APP_MANIFEST_SEARCH_URL}/${collectionName}?q=${searchParams.get('q')}`;
+            const windows = Object.values(store.getState().windows);
+            let firstWindow: any = windows.length > 0 ? windows[0] : null;
+            let companionWindowId: string = '';
+
+            // If there is no window yet, we need to create one
+            if (!firstWindow) {
+                store.dispatch(actions.addWindow({ manifestId: currentManifest.id }));
+            } else {
+                store.dispatch(actions.updateWindow(firstWindow.id, { manifestId: currentManifest.id }));
+            }
+            // After the window has been created or updated, we check for search parameters and open the companion window 'search' if necessary.
+            if (searchParams.has('q')) {
+                firstWindow = Object.values(store.getState().windows)[0];
+                companionWindowId = selectors.getCompanionWindowIdsForPosition(store.getState(), { position: 'left', windowId: firstWindow.id })[0];
+
+                // Open the companion window 'search'
+                store.dispatch(
+                    actions.addOrUpdateCompanionWindow(
+                        firstWindow.id,
+                        {
+                            content: 'search',
+                            position: 'left'
+                        }
+                    )
+                )
+                // Perform the search
+                store.dispatch(
+                    actions.fetchSearch(
+                        firstWindow.id,
+                        companionWindowId,
+                        searchUrl,
+                        searchParams.get('q')
+                    )    
+                )
+
+                // Info: Unfortunately, there is no other way to listen if the search has been performed...
+                //Subscribe to the redux store to get notified when the search results are available
+                const unsubscribe: Function = store.subscribe(async () => {
+                    const searchIsCompleted = Object.values(store.getState().searches).some((search: any) => Object.values(search).some((s: any) => !s?.data[searchUrl]?.isFetching && !!s?.selectedContentSearchAnnotationIds));
+
+                    if (searchIsCompleted) {
+                        await unsubscribe();
+
+                        // The timeout is necessary, to make sure the is canvas changed effectively
+                        setTimeout(() => {
+                            store.dispatch(
+                                actions.setCanvas(
+                                    firstWindow.id,
+                                    canvasId
+                                )
+                            );
+                        }, 100)
+                    }
+                })
+            }
+        }
+    }, [viewerInstance, currentManifest]); // eslint-disable-line react-hooks/exhaustive-deps
+
+    /**
+     * This method is executed when the component is mounted and when the window size changes
+     */
+    useEffect(() => {
+        // As we have less space on small screens, we need to adjust the mirador configuration to hide unnecessary elements
         if (viewerInstance) {
             const { store } = viewerInstance;
 
             store.dispatch(
-                actions.updateWindow(
-                    Object.keys(store.getState().windows)[0],
-                    {
-                        textOverlay: {
-                            enabled: !isMobile,
-                            visible: !isMobile,
-                        },
-                        sideBarOpen: !isMobile,
-                    }
-                )
+                actions.updateWindow(Object.keys(store.getState().windows)[0], {
+                    sideBarOpen: !isMobile,
+                })
             );
             store.dispatch(
-                actions.setWindowThumbnailPosition(
-                    Object.keys(store.getState().windows)[0], 
-                    isMobile ? 'off' : 'far-right',
-                )
+                actions.setWindowThumbnailPosition(Object.keys(store.getState().windows)[0], isMobile ? 'off' : 'far-right')
             );
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isMobile]);
 
-    useEffect(() => {
-        const changeLanguage = () => {
-            viewerInstance.store.dispatch(actions.updateConfig({
-                language: i18next.language,
-            }));
+    /**
+     * This method is executed as a callback when the i18next language changes
+     */
+     const changeLanguage = () => {
+        if (viewerInstance) {
+            const { store } = viewerInstance;
+            store.dispatch(
+                actions.updateConfig({
+                    language: i18next.language,
+                })
+            );
         }
-        const handler = (event: any) => setIsMobile(event.matches);
+    };
 
-        i18next.on('languageChanged', changeLanguage);
-        window.matchMedia("(max-width: 600px)").addEventListener('change', handler);
-
-        return () => {
-            i18next.off('languageChanged', changeLanguage);
-            window.matchMedia("(max-width: 600px)").removeEventListener('change', handler);
-        }
-    });
-
-    return (
-        <div id={'mirador-' + id.current.toString()} className="aiiif-mirador"></div>
-    );
+    // We need to return an empty div, to define the container for the mirador viewer. The container is linked by its id.
+    return <div id={`mirador-${id}`} className="aiiif-mirador" />;
 }
--- untyped.d.ts
+++ untyped.d.ts
@@ -2,6 +2,10 @@ declare module "mirador/dist/es/src/state/actions/index.js" {
     const foo:IFoo;
     export = foo;
 }
+declare module "mirador/dist/es/src/state/selectors/index.js" {
+    const foo:IFoo;
+    export = foo;
+}
 declare module "mirador" {
     const foo:IFoo;
     export = foo;
